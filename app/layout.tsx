960974d00e62bf34fb21030e1bc8ea094940d0aa
mujtaba chandio <160246068+mujtabachandio@users.noreply.github.com>
2024-11-17 05:57:22 +0500

    14.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        <Analytics/>
         {children}
+        <Analytics/>
         </body>
     </html>
   );
