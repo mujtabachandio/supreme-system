e7f4e5fbd960b36a8720f5447f1f914810336b7d
mujtaba chandio <160246068+mujtabachandio@users.noreply.github.com>
2024-12-03 22:43:24 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,31 +2,33 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mujtaba | Portfolio",
-  description: "Portfolio website created by Mujtaba Chandio on Nextjs ,tailwind css ,typescript",
+  description: "Portfolio website created by Mujtaba Chandio on Next.js, Tailwind CSS, TypeScript",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
       </head>
       <body className={inter.className}>
-        <Navbar/>
-        
+        <Navbar />
         {children}
-        <Analytics/>
-        </body>
+        <Analytics />
+      </body>
     </html>
   );
 }
